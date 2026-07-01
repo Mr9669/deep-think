a9c66dad2e74f7e4f1b350544f82be5899339191
Mr.69 <19821319669@163.com>
2026-07-02 00:25:54 +0800

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .deep_think import enhance, wrap_messages, inject_seed, quick_think, get_system_prompt
+
+__version__ = "0.1.0"
+__all__ = ["enhance", "wrap_messages", "inject_seed", "quick_think", "get_system_prompt"]
