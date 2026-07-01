f14a704ccb0945bea5bfddf32c3f3b54414813fd
Mr.69 <19821319669@163.com>
2026-07-02 00:49:37 +0800

    v0.1.1

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .deep_think import enhance, wrap_messages, inject_seed, quick_think, get_system_prompt
+from .deep_think import ReasoningEngine, FRAMEWORK, enhance
 
 __version__ = "0.1.0"
-__all__ = ["enhance", "wrap_messages", "inject_seed", "quick_think", "get_system_prompt"]
+__all__ = ["ReasoningEngine", "FRAMEWORK", "enhance"]
